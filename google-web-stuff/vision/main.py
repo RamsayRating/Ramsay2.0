60cb9b6e0ca4a08e1a20803c1c27dd48ee2b3a21
Elepert <emily.lepert@students.olin.edu>
2017-10-22 01:04:26 -0400

    updated main.py to change file paths

--- google-web-stuff/vision/main.py
+++ google-web-stuff/vision/main.py
@@ -2,80 +2,46 @@ import base64
 import os
 
 from flask import Flask, redirect, render_template, request
-from google.cloud import datastore
-from google.cloud import storage
-from google.cloud import vision
 
+from werkzeug.utils import secure_filename
+
+#CHANGE THIS TO YOUR OWN PATH
+UPLOAD_FOLDER = '/Users/emilylepert/Documents/Olin_2/HackHarvard/Ramsay2.0/resources'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def homepage():
-    # Create a Cloud Datastore client.
-    datastore_client = datastore.Client()
-
-    # Use the Cloud Datastore client to fetch information from Datastore about
-    # each photo.
-    query = datastore_client.query(kind='Photos')
-    image_entities = list(query.fetch())    
-
     # Return a Jinja2 HTML template.
     return render_template('homepage.html', image_entities=image_entities)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload_photo', methods=['GET','POST'])
 def upload_photo():
     # Create a Cloud Storage client.
-    '''
-    storage_client = storage.Client()
-
-    # Get the Cloud Storage bucket that the file will be uploaded to.
-    bucket = storage_client.get_bucket(os.environ.get('CLOUD_STORAGE_BUCKET'))
-
-    # Create a new blob and upload the file's content to Cloud Storage.
-    photo = request.files['file']
-    blob = bucket.blob(photo.filename)
-    blob.upload_from_string(
-            photo.read(), content_type=photo.content_type)
-
-    # Make the blob publicly viewable.
-    blob.make_public()
-    image_public_url = blob.public_url
     
-    # Create a Cloud Vision client.
-    vision_client = vision.ImageAnnotatorClient()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    # Retrieve a Vision API response for the photo stored in Cloud Storage
-    source_uri = 'gs://{}/{}'.format(os.environ.get('CLOUD_STORAGE_BUCKET'), blob.name)
-    response = vision_client.annotate_image({
-        'image': {'source': {'image_uri': source_uri}},
-    })
-    labels = response.label_annotations
-    faces = response.face_annotations
-    web_entities = response.web_detection.web_entities
-
-    # Create a Cloud Datastore client
-    datastore_client = datastore.Client()
-
-    # The kind for the new entity
-    kind = 'Photos'
-
-    # The name/ID for the new entity
-    name = blob.name
-
-    # Create the Cloud Datastore key for the new entity
-    key = datastore_client.key(kind, name)
-
-    # Construct the new entity using the key. Set dictionary values for entity
-    # keys image_public_url and label.
-    entity = datastore.Entity(key)
-    entity['image_public_url'] = image_public_url
-    entity['label'] = labels[0].description
-
-    # Save the new entity to Datastore
-    datastore_client.put(entity)
-    '''
-    # Redirect to the home page.
     return render_template('secondpage.html')
 
 
