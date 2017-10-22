6add5feb8ca4a797e8109c15874b5e353ca3d428
Sam Eppinger <samantha.eppinger@students.olin.edu>
2017-10-21 20:22:34 -0400

    Little commit message

--- food_recognition.py
+++ food_recognition.py
@@ -29,5 +29,27 @@ def what_object(img_name):
     # Gets first element
     return labels[0].score
 
+def stars(img_name):
+    corr_val = what_object(img_name)
+    star = 0
+
+    if (corr_val <= .6):
+        return(1, corr_val)
+        #print("1 Star")
+    elif (.6 < corr_val) and (corr_val <= .7):
+        return(2, corr_val)
+        #print("2 Star")
+    elif (.7 < corr_val) and (corr_val <= .8):
+        return(3, corr_val)
+        #print("3 Star")
+    elif (.8 < corr_val) and (corr_val <= .9):
+        return(4, corr_val)
+        #print("4 Star")
+    else:
+        return(5, corr_val)
+        #print("5 Star")
+
 if __name__ == '__main__':
-    print('Top score: ' + str(what_object('RedApple.jpg')))
+    star_val, corr_val = stars('RedApple.jpg')
+    print('Top score: ' + str(corr_val))
+    print('Star Value: ' + str(star_val))
